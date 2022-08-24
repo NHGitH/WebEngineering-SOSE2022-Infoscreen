ffb6f973e74b87ca296ac53809a4a4a7121c7bd9
NHGitH <niklas.horn4handball@googlemail.com>
2022-08-24 17:27:31 +0200

    Update layout.blade.php
    test -redo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,8 +26,6 @@
 
         {{$slot}}
 
-        <x-test-html>
-
         <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
