7ef8d272de3109f2c71fa36690bf85e6a776bf70
NHGitH <niklas.horn4handball@googlemail.com>
2022-08-26 16:17:27 +0200

    Erstellen der SQL Tabellen
    +Raum
    +Gebäude
    +Professor
    +Modul
    +Studiengang

--- database/migrations/2022_08_26_135231_create_professoren_table.php
+++ database/migrations/2022_08_26_135231_create_professoren_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCommentsTable extends Migration
+class CreateProfessorenTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('professoren', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('picture_path');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('professoren');
     }
 }
