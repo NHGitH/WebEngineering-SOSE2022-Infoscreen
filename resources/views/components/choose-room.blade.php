5239884c62fbf6b33d94e3c05a488209a9befe47
NHGitH <niklas.horn4handball@googlemail.com>
2022-09-15 18:58:54 +0200

    Update choose-room.blade.php

--- resources/views/components/choose-room.blade.php
+++ resources/views/components/choose-room.blade.php
@@ -5,7 +5,7 @@
         <input type="text" id="building" name="building" value=""/>
         <label for="room">Raum:</label>
         <input type="number" id="room" name="room" value=""/>
-        <a href='' onclick="this.href='/'+document.getElementById('building').value+'/rooms/'+document.getElementById('room').value">Seite laden</a>
+        <a href='' onclick="this.href='/buildings/'+document.getElementById('building').value+'/'+document.getElementById('room').value">Seite laden</a>
     </form>
 
 </div>
